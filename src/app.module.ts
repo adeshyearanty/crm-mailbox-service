b60faf84e8be0dd81887b3f86a39d650f080dc01
Yearanty Sri Sai Adesh <adeshy@mirakitech.com>
2025-08-12 21:33:02 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,9 @@ import { CalendarModule } from './calendar/calendar.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      envFilePath: `.env`,
       isGlobal: true,
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'production', 'staging')
-          .default('development'),
         PORT: Joi.number().default(3006),
         MONGO_USER: Joi.string().required(),
         MONGO_PASSWORD: Joi.string().required(),
